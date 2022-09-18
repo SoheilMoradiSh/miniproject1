83b8838b8724d70791f1e1985fbc78864059f80d
soheil <soheil0000moradi@gmail.com>
2022-09-18 23:16:31 +0430

    part3

--- src/timer.js
+++ src/timer.js
@@ -1,36 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './style.css'
+import "./style.css";
 
 var interval;
-class Timer extends React.Component{
-  constructor(){
+class Timer extends React.Component {
+  constructor() {
     super();
     this.state = {
-      time : 10
-    }
+      second: 0,
+      minute: 0,
+      hour: 0,
+      isStart: false,
+    };
   }
-  componentDidMount(){
-      interval=setInterval(()=>{
+  // componentDidMount(){
+  //     interval=setInterval(()=>{
+  //       this.setState({
+  //         time:this.state.time - 1
+  //       })
+  //     } , 1000)
+  // }
+  // componentDidUpdate(){
+  //   if(this.state.time===0){
+  //     clearInterval(interval)
+  //   }
+  // }
+  stop = () => {
+    clearInterval(interval);
+  };
+  start = () => {
+    if (this.state.isStart == false) {
+      interval = setInterval(() => {
         this.setState({
-          time:this.state.time - 1
-        })
-      } , 1000)
-  }
-  componentDidUpdate(){
-    if(this.state.time===0){
-      clearInterval(interval)
+          second: this.state.second + 1,
+        });
+        if (this.state.second == 60) {
+          this.setState({
+            second: 0,
+            minute: this.state.minute + 1,
+          });
+        }
+        if (this.state.minute == 60) {
+          this.setState({
+            minute: 0,
+            hour: this.state.hour + 1,
+          });
+        }
+      }, 1000);
     }
-  }
-  render(){
-    return(
-      <h2 className="timer">
-        it is {this.state.time}
-      </h2>
-    )
+  };
+  reset = () => {
+    this.setState({
+      second: 0,
+      minute: 0,
+      hour: 0,
+      isStart: false,
+    });
+  };
+  render() {
+    var h = this.state.hour;
+    var m = this.state.minute;
+    var s = this.state.second;
+    return (
+      <>
+        <h2 className="timer">
+          {`${h > 9 ? h : "0" + h} : ${m > 9 ? m : "0" + m} : ${
+            s > 9 ? s : "0" + s
+          } `}
+        </h2>
+        <div className="btn">
+          <button onClick={this.stop} className="mybtn">Stop</button>
+          <button onClick={this.start} className="mybtn">Start</button>
+          <button onClick={this.reset} className="mybtn">Reset</button>
+        </div>
+      </>
+    );
   }
 }
 
 export default Timer;
-
